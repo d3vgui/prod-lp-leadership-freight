c90833bac81fd965ba6a0b73896655b2a52bdcb6
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-05-30 22:26:38 -0300

    config contact form

--- js/contact-form.js
+++ js/contact-form.js
@@ -32,7 +32,7 @@ document.querySelector('#contactForm').addEventListener('submit', async function
   }, 400);
 
   try {
-    const response = await fetch('https://lfgex.com:3000/send-email', {
+    const response = await fetch('/send-email', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
