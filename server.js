2da50af4f3915d35eb237134628f1bccf93f9ef3
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-05-30 21:15:58 -0300

    config server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
